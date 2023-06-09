4ef628874b21a4a76e744b4138c84c09ca5a8b3e
Huzaifa Sserugo <sseruzaifa@gmail.com>
2023-06-09 10:34:03 +0300

    Update About.js
    disabled the techstack and toolstack by commenting them out

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -38,12 +38,12 @@ function About() {
           Professional <strong className="purple">Skillset </strong>
         </h1>
 
-        <Techstack />
+        {/* <Techstack /> */}
 
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
-        <Toolstack />
+        {/* <Toolstack /> */}
 
         <Github />
       </Container>
